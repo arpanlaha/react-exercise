6b70eeefd0f43e5f6b26236385b02b36b0b96cad
Arpan Laha <alaha2@illinois.edu>
2018-09-17 22:09:10 -0500

    finished part 3 and 4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react'
 import { Instructions } from './components'
 import { Counter } from './components'
+import { UserInput } from './components'
 import './styles/app.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { count: 0, submission: 0 }
+    this.getInput = this.getInput.bind(this)
+    this.checkInput = this.checkInput.bind(this)
+    this.setCount = this.setCount.bind(this)
+  }
+
+  getInput(event) {
+    this.setState({ submission: event.target.value })
+  }
+
+  checkInput(event) {
+    this.setState({ submission: Number(this.state.submission) })
+    if (isNaN(this.state.submission)) {
+      alert('Not a number!')
+    } else {
+      this.setCount(this.state.submission)
+    }
+  }
+
+  setCount(value) {
+    this.setState({ count: value })
+  }
+
   render() {
     return (
       <div className="app">
@@ -12,7 +38,10 @@ class App extends Component {
           shouldDisplayImage={true}
           items={['apple', 'banana', 'clementine', 'dragonfruit', 'bl']}
         />
-        <Counter count={0} />
+        <br />
+        <Counter count={Number(this.state.count)} setCount={this.setCount} />
+        <br />
+        <UserInput getInput={this.getInput} checkInput={this.checkInput} />
       </div>
     )
   }
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
 import Instructions from './Instructions.component'
 import Counter from './Counter.component'
+import UserInput from './UserInput.component'
 
 export { Instructions }
 export { Counter }
+export { UserInput }
